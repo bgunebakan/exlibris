fc168fca17a1897775193d57cae9cc2e466d07b1
bgunebakan <bilaltonga@gmail.com>
2021-10-26 03:43:29 +0300

    added create tag feature

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -6,7 +6,9 @@ from core.models import Tag
 from catalog import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -17,3 +19,8 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         """Return objects for the current authenticated user only"""
 
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+
+        serializer.save(user=self.request.user)
