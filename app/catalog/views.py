92e803cc41b1110d1e1b15cf48b023da3466bd46
bgunebakan <bilaltonga@gmail.com>
2021-10-28 01:42:37 +0300

    refactor viewsets

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -6,42 +6,33 @@ from core.models import Tag, Book
 from catalog import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseCatalogAttrViewset(viewsets.GenericViewSet,
+                             mixins.ListModelMixin,
+                             mixins.CreateModelMixin):
+    """Base viewset for user owned catalog attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
 
-        return self.queryset.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        """Create a new tag"""
-
-        serializer.save(user=self.request.user)
-
-
-class BookViewSet(viewsets.GenericViewSet,
-                  mixins.ListModelMixin,
-                  mixins.CreateModelMixin):
-    """Manage books in the database"""
-
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-    queryset = Book.objects.all()
-    serializer_class = serializers.BookSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user"""
-
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new book"""
+        """Create a new object"""
 
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseCatalogAttrViewset):
+    """Manage tags in the database"""
+
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class BookViewSet(BaseCatalogAttrViewset):
+    """Manage books in the database"""
+
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
